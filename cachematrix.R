9b02682bbe25944d1cd49ca63ca2f2333ff53f93
Brian Wang <fancheng_wang@hotmail.com>
2017-08-07 17:36:44 +0800

    Second commit of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,27 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## 'makeCacheMatrix' is used to set and get a square matrix, it's "matrix(1:4,nrow=2,ncol=2)" in the example:
+
+## 'cacheSolve' is used to get the inverse of the matrix that 'makeCacheMatrix' generates. If the inverse of the matrix (r) has already been computed (r is not NULL), then just return it, if not, then calculate the inverse of the matrix and show it. If 'cacheSolve' runs once, then the inverse of the matrix is saved in the cache. Next time run 'cacheSolve', the inverse of the matrix (r) will be returned from cache immediately.
+
+## > source('F:/Education/Git & R/R/Sources/cachematrix.R')
+## > makeCacheMatrix()$set(matrix(1:4,nrow=2,ncol=2))
+## > cacheSolve(makeCacheMatrix())
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(makeCacheMatrix())
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(makeCacheMatrix())
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function() {
