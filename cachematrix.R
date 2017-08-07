cd52ec50405956a84b3cb094d4af806b2026e802
Brian Wang <fancheng_wang@hotmail.com>
2017-08-07 17:23:30 +0800

    First commit of cachemstrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,26 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function() {
+  set <- function(y) {
+    x <<- y
+    r <<- NULL
+  }
 
+  get <- function() x
+  list(set = set, get = get)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if(!is.null(r)) {
+     message("getting cached data")
+     return(r)
+  }
+  my_matrix <- x$get()
+  r <<- solve(my_matrix, ...)
+  r
 }
